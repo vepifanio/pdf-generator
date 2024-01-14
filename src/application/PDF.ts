b3c512a63c191befa77651a2bced7360a89c3bbe
vepifanio <vepifanio1@gmail.com>
2024-01-14 18:51:33 -0300

    feat: add `title` property | add `resolveContent` method

--- src/application/PDF.ts
+++ src/application/PDF.ts
@@ -2,6 +2,7 @@ import { randomUUID } from 'node:crypto';
 
 interface PDFProps {
   id?: string;
+  title: string;
   content: string;
   backgroundImageUrl?: string;
   textColor?: string;
@@ -10,6 +11,7 @@ interface PDFProps {
 
 export class PDF {
   id: string;
+  title: string;
   content: string;
   backgroundImageUrl?: string;
   textColor?: string;
@@ -17,15 +19,38 @@ export class PDF {
 
   constructor({
     id,
+    title,
     content,
     backgroundImageUrl,
     textColor,
     createdAt,
   }: PDFProps) {
     this.id = id || randomUUID();
+    this.title = title;
     this.content = content;
     this.backgroundImageUrl = backgroundImageUrl;
     this.textColor = textColor;
     this.createdAt = createdAt || new Date();
+  }
+
+  resolveContent(contentVariables: { [key: string]: string } = {}): string {
+    const contentTextResolved = Object.keys(contentVariables).reduce(
+      (text, key) => {
+        return text.replace(`{{${key}}}`, contentVariables[key]);
+      },
+      this.content,
+    );
+
+    const remainingVariables = contentTextResolved.match(/{{(\w+)}}/gim);
+
+    if (remainingVariables) {
+      const errorText = `Some required content variables are undefined: ${remainingVariables.join(
+        ', ',
+      )}`;
+
+      throw new Error(errorText);
+    }
+
+    return contentTextResolved;
   }
 }
